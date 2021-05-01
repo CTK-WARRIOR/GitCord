a2e06f6b0fceb8d4dc100c914a06baebdad8b672
CTK WARRIOR <>
2021-05-01 13:44:08 +0000

    🌹 Fixed wrong URL issue

--- src/Events/PullRequestEvent.js
+++ src/Events/PullRequestEvent.js
@@ -5,7 +5,7 @@ module.exports = (json, { color } = {}) => {
     color: color ? color : (colorObject[json.payload.action] ? colorObject[json.payload.action] : "GREEN"),
     author: { name: json.actor.login, icon_url: json.actor.avatar_url },
     title: `[${json.repo.name}] Pull request ${json.payload.action} #${json.payload.pull_request.number}: ${json.payload.pull_request.title}`,
-    url: json.payload.pull_request.url,
+    url: json.payload.pull_request.html_url,
     description: json.payload.action.includes("opened") ? json.payload.pull_request.body : null
   }
 }
