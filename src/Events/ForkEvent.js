a5d4767bc566baeb420d8acda6a58300f55246de
CTK WARRIOR <>
2021-05-03 18:45:51 +0000

    🤦‍♂️ Invalid URL

--- src/Events/ForkEvent.js
+++ src/Events/ForkEvent.js
@@ -3,7 +3,7 @@ module.exports = (json, { color = "BLUE" } = {}) => {
     color,
     author: { name: json.actor.login, icon_url: json.actor.avatar_url },
     title: `[${json.repo.name}] Fork Created : ${json.payload.forkee.full_name}`,
-    url: json.payload.forkee.url,
+    url: json.payload.forkee.html_url,
     footer: { text: "Github" },
     timestamp: new Date()
   }
